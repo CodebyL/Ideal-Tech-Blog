44f5c799f9024303b9193d0b50357d3056689755
Lisette Colon <lisettecolon23@gmail.com>
2021-09-04 00:54:37 -0400

    jb files added

---
+++ js/comment.js
@@ -0,0 +1,26 @@
+const createCommentHandler = async (event) => {
+    event.preventDefault();
+  
+    const post_id = document.location.pathname.split('/')[2];
+    const comment = document.querySelector('#comment-content').value.trim();
+    console.log('comment and post_id:', comment, post_id);
+  
+    if (comment && post_id) {
+      const response = await fetch('/api/comments', {
+        method: 'POST',
+        body: JSON.stringify({ comment, post_id }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  console.log(document.location.pathname.split('/')[2]);
+  
+  // browser event handler
+  document
+    .querySelector('#comment-btn')
+    .addEventListener('click', createCommentHandler);
---
+++ js/post-edit.js
@@ -0,0 +1,44 @@
+const updatePostHandler = async (event) => {
+    event.preventDefault();
+  
+    const id = window.location.toString().split('/').slice(-1)[0];
+    const title = document.querySelector('#post-title').value.trim();
+    const content = document.querySelector('#post-content').value.trim();
+    
+  
+    console.log("title, conent:", title, content);
+    
+    if (id && title && content) {
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ title, content }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+  
+      if (response.ok) {
+        document.location.replace(`/dashboard`);
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+      if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+    
+        const response = await fetch(`/api/posts/${id}`, {
+          method: 'DELETE',
+        });
+    
+        if (response.ok) {
+          document.location.replace('/dashboard');
+        } else {
+          alert('Failed to delete the post');
+        }
+      }
+    };
+  
+    document.querySelector('#update-post-btn').addEventListener('click', updatePostHandler);
+    document.querySelector('#delete-post-btn').addEventListener('click', delButtonHandler);
